bfc9f47cac83d3a63541a9faf9fe94b5393e98fa
jabeel shahis ak <jabeelshahisak@gmail.com>
2023-11-17 09:28:46 +0530

    homeview creted

--- ecommapp/views.py
+++ ecommapp/views.py
@@ -1,10 +1,12 @@
+from typing import Any
 from django.shortcuts import render
-from django.views.generic import CreateView,FormView
+from django.views.generic import CreateView,FormView,TemplateView,DetailView
 from django.contrib.auth.models import User
 from django.urls import reverse_lazy
 from ecommapp.forms import SignUpForm,SignInForm
 from django.contrib import messages
 from django.contrib.auth import authenticate,login,logout
+from ecommapp.models import Products
 
 # Create your views here.
 
@@ -32,3 +34,20 @@ class SignIn(FormView):
                 msg="invalid credentials"
                 messages.error(request,msg)
                 return render (request,"signin.html")
+            
+class HomeView(TemplateView):
+    template_name="homehtml"
+
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        all_products=Products.objects.all()
+        context['products']=all_products
+        return context
+        
+        
+
+class ProductDetilView(DetailView):
+    model =Products
+    template_name="productdetail.html"
+    pk_url_kwarg="id"
+    context_object_name="product"
